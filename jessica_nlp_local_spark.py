bc4685e91d24aa618b39da8f064bfc6f8a90ff1f
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-07-10 22:22:27 +0400

    Add files via upload

---
+++ jessica_nlp_local_spark.py
@@ -0,0 +1,12 @@
+#########jessica_nlp_local_spark.py###########
+
+import pyspark
+from pyspark import *
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+sc = SparkContext("local")
+sqlContext = SparkSession.builder.getOrCreate()
+
+#########jessica_nlp_local_spark.py###########
