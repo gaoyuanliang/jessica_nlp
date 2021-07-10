1a96e82c759307807873185591718e282766e4ff
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2021-07-10 22:15:44 +0400

    Delete jessic_nlp_local_spark.py

--- jessic_nlp_local_spark.py
+++
@@ -1,8 +0,0 @@
-import pyspark
-from pyspark import *
-from pyspark.sql import *
-from pyspark.sql.types import *
-from pyspark.sql.functions import *
-
-sc = SparkContext("local")
-sqlContext = SparkSession.builder.getOrCreate()
